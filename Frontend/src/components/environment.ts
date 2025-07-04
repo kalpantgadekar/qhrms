74be393f5590e24b1be0bbf346dc1e430aa9b902
Kalpant Gadekar <144586245+kalpantgadekar@users.noreply.github.com>
2025-07-04 19:57:54 +0530

    Update environment.ts

--- Frontend/src/components/environment.ts
+++ Frontend/src/components/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,  // ✅ Since you're deploying this
-  apiBaseUrl: 'http://3.90.235.184:8080/api',  // 🔁 Replace with your backend IP/domain
+  apiBaseUrl: 'http://18.133.249.41:8080/api',  // 🔁 Replace with your backend IP/domain
   appName: 'HRMS',
   enableDebug: false
 };
